fbf9c361961559d64fa90664c5a4ba5b25974efc
N Rowe <naterogen@outlook.com>
2021-06-16 14:05:02 -0400

    squashing navigation [last 5 commits] to undo my undo on main
    Created a basic layout for the app. Added two navigation stacks encapsulated in a tab navigator. Prepared environment to handle authentication.
    Huge organizational overhaul
    
    * Moved screen-like components to /screens, small useful components to /components, and styles to /config.
    + Also created a dummy `settings.js` file.
    * Relocated images from `assets` folder to `public`.
    - Deleted `Screens.js` in favor of making code more modular.
    + Created an `index.js` in `screens` that serves as the main entry point for any future usage of screen components.
    added newlines to file ends to appease Lord Github
    
    Fixed export defaults for web client
    
    dunno why it didn't take my syntax but it should x-platform now
    minor text update

---
+++ screens/CreateReminder.js
@@ -0,0 +1,14 @@
+//CreateReminder screen
+import React from 'react';
+import { Text } from 'react-native';
+import ScreenContainer from '../components/ScreenContainer';
+
+const CreateReminder = ({ navigation }) => {
+  return (
+    <ScreenContainer>
+      <Text>Create a Reminder</Text>
+    </ScreenContainer>
+  )
+}
+
+export default CreateReminder;
