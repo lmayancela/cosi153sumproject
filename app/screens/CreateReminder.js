bc0b3ef509e65325c3640437209c20408c1c52bd
arisrivaapple <44928754+arisrivaapple@users.noreply.github.com>
2021-06-20 16:07:00 -0400

    MVP addTask & taskList
    both are on one page and functional, using async storage

--- app/screens/CreateReminder.js
+++ app/screens/CreateReminder.js
@@ -1,18 +1,54 @@
 //CreateReminder screen
-import React from 'react';
-import { Text } from 'react-native';
+import React, {useState} from 'react';
+import { SafeAreaView, ScrollView, View, Button,
+         FlatList, StyleSheet, Text, TextInput, StatusBar } from 'react-native';
 import ScreenContainer from '../components/ScreenContainer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+//Notes:
+//cant scrool down in browser (text can get cut off)
+//make it so theres a error if yuo try to submit a task without a task Name
+//make the tasks sort by due date?
+
+//I DONT KNOW IF WE CAN USE THE PROPS FINCTIONALITY AND THE NAVIGATION AT THE SAME TIME
+//but also this page seems to be working properly?
 const CreateReminder = ({ navigation }) => {
+  const [taskDueDate, setTaskDueDate] = useState(""); //blank date?
+  const [notificationType, setNotificationType] = useState([]); //keeping this as general rather than per task for now
+  const [notificationFrequency, setNotificationFrequency] = useState("");
+  const [taskName, setTaskName] = useState("");
+  const [taskNotes, setTaskNotes] = useState("");
+  const [task, setTask] = useState({taskName:'', taskDueDate:'', notificationType:'', notificationFrequency:''});
+  const [taskList, setTaskList] = useState([]);
+
+  const [reactionText, setReactionText] = useState("");
+
+  const renderTask = ({item}) => {
+      return (
+        <View>
+             <Text>Task: {item.taskName} </Text>
+             <Text> Due Date: {item.taskDueDate} </Text>
+             <Text> Notifcation Type: {item.notificationType} </Text>
+             <Text> Notification Frequency: {item.notificationFrequency} </Text>
+             <Text> Notes: {item.taskNotes} </Text>
+        </View> //make about to edit by clicking on fields? have task details expand/shrink?
+      )
+    }
+
   return (
     <ScreenContainer>
-      <Text>Create a Reminder</Text>
-      <Text style={styles.header}>Sign Up Form</Text>
+    <View>
+      <Text style={styles.headerText}>Create a Reminder</Text>
+      <Text> </Text>
       <View style={styles.rowContainer}>
         <Text>What is the task you want to complete?</Text>
         <TextInput
           style={styles.textinput}
-          onChangeText={text => { setTaskName(text) }}
+          onChangeText={text => {
+            setTaskName(text);
+            setReactionText("");
+          }}
+          value = {taskName}
         />
       </View>
       <View style={styles.rowContainer}>
@@ -20,6 +56,7 @@ const CreateReminder = ({ navigation }) => {
         <TextInput
           style={styles.textinput}
           onChangeText={text => { setTaskDueDate(text) }}
+          value = {taskDueDate}
         />
       </View>
       <View style={styles.rowContainer}>
@@ -27,7 +64,7 @@ const CreateReminder = ({ navigation }) => {
         <TextInput
           style={styles.textinput}
           onChangeText={text => { setNotificationType(text) }}
-          value={null} //tbh dont know the purpose of this line //NR: added null just to avoid compilation errors
+          value = {notificationType}
         />
       </View>
       <View style={styles.rowContainer}>
@@ -35,7 +72,7 @@ const CreateReminder = ({ navigation }) => {
         <TextInput
           style={styles.textinput}
           onChangeText={text => { setNotificationFrequency(text) }}
-          value={null} //ditto above
+          value={notificationFrequency}
         />
       </View>
       <View style={styles.rowContainer}>
@@ -43,11 +80,69 @@ const CreateReminder = ({ navigation }) => {
         <TextInput
           style={styles.textinput}
           onChangeText={text => { setTaskNotes(text) }}
-          value={null} //ditto above
+          value={taskNotes}
         />
+      </View>
+      <View>
+        <Button
+           title={"Add Item"}
+           onPress = {() => {
+             //do i have to declare a new task?
+             setTask({'taskName':taskName, 'taskDueDate':taskDueDate, 'notificationType':notificationType, 'notificationFrequency':notificationFrequency, 'taskNotes':taskNotes})
+             //i use a seperate step to make the task as opposed to what we did in class bc I thik  its cleaer
+             setTaskList(taskList.concat(
+               {'taskName':taskName, 'taskDueDate':taskDueDate, 'notificationType':notificationType, 'notificationFrequency':notificationFrequency, 'taskNotes':taskNotes}))
+            setReactionText("Task " + taskName + " successfully added!!")
+             setTaskName("")
+             setTaskDueDate("")
+             setNotificationType("")
+             setNotificationFrequency("")
+             setTaskNotes("")
+           }}
+           />
+      <Text> </Text>
+      </View>
+      <Text> </Text>
+      {reactionText}
+      <Text> </Text>
+      <Text> </Text>
+      <Text style={styles.headerText}>Your Tasks:</Text>
+      <FlatList
+        data={taskList}
+        renderItem={renderTask}
+        keyExtractor={item => item.taskName} //i dont know what this line does
+      />
+      <Text>
+
+       todoItems is {JSON.stringify(taskList)}
+       </Text>
       </View>
     </ScreenContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'column',
+    backgroundColor: '#eee',
+    alignItems: 'left',
+    justifyContent: 'left',
+    textAlign:'left',
+    marginTop:20,
+    padding:20,
+  },
+  todoItem:{
+    justifyContent:'left',
+  },
+  headerText: {
+    textAlign:'center',
+    backgroundColor:'#aaa',
+    fontSize: 16,
+    padding:10,
+    color: 'blue'
+  },
+
+});
 
 export default CreateReminder;
