9f671774d1c6f9158793318a54f7b228209e06fc
Lisandro Mayancela <lmayancela08@gmail.com>
2021-06-24 10:16:51 -0400

    Internal clock and sending em emails on time

--- app/screens/SendEmail.js
+++ app/screens/SendEmail.js
@@ -5,24 +5,75 @@ import {
   TextInput
 } from 'react-native';
 import ScreenContainer from '../components/ScreenContainer';
+import Clock from '../components/Clock';
+import Twitter from 'twitter';
 import emailjs from 'emailjs-com';
 emailjs.init("user_GjmuzdP6EsNTAhMiM9BPq")
 // import serialportgsm from 'serialport-gsm';
+const sampleData = new Map();
+sampleData.set('9:9', [{email: 'lmayancela08@gmail.com', task: 'apply for jobs'}])
+sampleData.set('9:2', [{email: 'lmayancela08@gmail.com', task: 'buy groceries'}])
+sampleData.set('9:30', [{email: 'lmayancela08@gmail.com', task: 'do laundry', name: 'Lisandro'}, {email: 'arisrivaapple@brandeis.edu', task: 'go on a walk', name: 'Aris'}])
+
 
 const SendEmail = ({ navigation }) => {
   const [email, setEmail] = useState("")
+  const [handle, setHandle] = useState("")
   const [task, setTask] = useState("")
   const [name, setName] = useState("")
   const [num, setNum] = useState("")
+  const [time, setTime] = useState("");
   let TEMPLATE_ID = 'template_11o4zg9'
   let SERVICE_ID = 'botherme_emails'
 
-  async function sendEmail() {
-    // console.log(await init("user_GjmuzdP6EsNTAhMiM9BPq"));
+  let client = new Twitter({
+    consumer_key: 'BMSjdsHWhAi3iw3YT8IkaeGnJ',
+    consumer_secret: '1AfJIv7n1WsGlVoOfcMX5pq6CQtIm6tTKHQpCqYxQljdA9M6fw',
+    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAHveQwEAAAAA700c%2Bi42tfXKe%2BMVvoz8RRR2KO0%3Dt5dMF7dt4BtMYmkzrFi4ulYPNW8zLqnOZ5JLRtlaVXKZViqUWD',
+  });
+
+  const postTweet = () => {
+    let params = {
+      type: 'message_create',
+      message_create:{
+        target: {
+          recipient_id: 'LisandroMayanc1',
+        },
+        message_data: {
+          text: task,
+        },
+      },
+    }
+
+    client.post('statuses/update', {status: 'I am a tweet'},  function(error, data, response) {
+      if(error) throw error;
+      console.log(data);  // Tweet body.
+      console.log(response);  // Raw response object.
+    });
+  }
+
+
+  // const twitter_params = {
+  //   api_key: 'BMSjdsHWhAi3iw3YT8IkaeGnJ',
+  //   api_key_secret: '1AfJIv7n1WsGlVoOfcMX5pq6CQtIm6tTKHQpCqYxQljdA9M6fw',
+  //   bearer_token: 'AAAAAAAAAAAAAAAAAAAAAHveQwEAAAAA700c%2Bi42tfXKe%2BMVvoz8RRR2KO0%3Dt5dMF7dt4BtMYmkzrFi4ulYPNW8zLqnOZ5JLRtlaVXKZViqUWD',
+  // }
+
+  const timeCallback = (new_time) => {
+    console.log("Setting time to: " + new_time);
+    setTime(new_time);
+    if (sampleData.has(new_time)){
+      let reminders = sampleData.get(new_time)
+      reminders.forEach(reminder => sendEmail(reminder))
+    }
+  }
+
+  async function sendEmail(reminder) {
+    console.log("Sending email for reminder");
     const templateParams = {
-      task_name: { task }.task,
-      to_name: { name }.name,
-      to_email: { email }.email
+      task_name: reminder.task,
+      to_name: reminder.name,
+      to_email: reminder.email
     };
 
     await emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams)
@@ -33,14 +84,10 @@ const SendEmail = ({ navigation }) => {
       });
   }
 
-  // function sendText() {
-  //   serialportgsm.list((err, result) => {
-  //     console.log(result)
-  //   })
-  // }
-
   return (
     <ScreenContainer>
+      <Clock parentCallback = {timeCallback} />
+      <Text>Curr time is: {time} </Text>
       <Text>Enter an email address</Text>
       <TextInput onChangeText={text => { setEmail(text) }} />
       <Text>Enter a task</Text>
@@ -53,6 +100,11 @@ const SendEmail = ({ navigation }) => {
         color='red' title='Send Email'
         onPress={() => sendEmail()}
       />
+      <Button
+        color='blue' title='Post Tweet'
+        onPress={() => postTweet()}
+      />
+
       {/* <Button
         color='blue' title='Send Text'
         onPress={() => sendText()}
